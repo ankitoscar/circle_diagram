9daa470a207692f8a064d442989d2db56cdee321
ankitoscar <ankitoscar911@gmail.com>
2021-05-01 22:49:06 +0530

    added values dialog box

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import sys 
 from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QWidget
 from mainWindow import * 
+from calculatedValues import *
+import numpy as np  
 
 class Window(QWidget):
 
@@ -13,6 +15,7 @@ class Window(QWidget):
 
     def launchPopUp(self):
         pop = Popup(self)
+        pop.displayValues()
         pop.show()   
 
 
@@ -20,9 +23,22 @@ class Popup(QDialog):
     
     def __init__(self, parent):
         super().__init__(parent)
-        self.resize(600, 300)
-        self.label = QLabel('Pop Up Window', self)
+        self.ui = Ui_Dialog()
+        self.parent = parent
+        self.ui.setupUi(self)
+        self.show()
+    
+    def displayValues(self):
+        pfA = self.parent.ui.lineEdit_3.text()
+        pfB = self.parent.ui.lineEdit_4.text()
 
+        phi_A = round(np.arccos(np.float(pfA)) * 180 / np.pi, 2)
+        phi_B = round(np.arccos(np.float(pfB)) * 180 / np.pi, 2)
+
+        phi_A_value = self.ui.label.text()
+        phi_B_value = self.ui.label_2.text()
+        self.ui.label.setText(phi_A_value + str(phi_A))
+        self.ui.label_2.setText(phi_B_value + str(phi_B))
 if __name__=="__main__":
 
     app = QApplication(sys.argv)
